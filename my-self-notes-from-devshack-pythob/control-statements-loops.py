10d3d3efc19a4e25430b7e7e4e3d2640dff0d96f
ashok <ashokreddyothers@gmail.com>
2024-01-12 21:58:46 +0530

    d

---
+++ my-self-notes-from-devshack-pythob/control-statements-loops.py
@@ -0,0 +1,143 @@
+
+# a = input("hi user, enter your  name: ")
+# print ('user  entered  name:',a)
+
+
+# a = int(input("hi user, enter your  name: "))
+# print ('user  entered  name:',a)                  $ if you enter  non integer , then i/p is invalid if you give "int" as input
+
+# a = float(input("hi user, enter your  name: "))
+# print ('user  entered  name:',a)                   # other than float it give error
+
+
+# @@@@@@@@@@@@ QUESTION @@@@@@@@@@@@@@@@@@
+
+# HOW YOU TAKE INPUT FROM USER?
+#  by using input method as shown above
+ 
+#  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+# age=22
+# if age>= 18:
+#     print("user is adult")
+# else:
+#     print("user is not adult")
+ 
+
+# age=10
+# if age>= 18:
+#     print("user is adult")
+# else:
+#     print("user is not adult")
+    
+    
+# age=int(input("enter your age: "))
+# if age>= 18:
+#     print("user is adult")
+# else:
+#     print("user is not adult")
+
+
+############################################  IF - ELIF #############################################
+
+# score=int(input("enter your scor "))
+# if score >= 90:
+#     print("user is distinction")
+# elif score >= 80:
+#     print("user is grade-B")
+# elif score >= 70:
+#     print("grade-c")
+# else:
+#     print ("user is fail")
+   
+    
+
+ 
+############################################  IF - nested-if #############################################
+
+# x=30   # 30,10,40
+# y=20
+# if x>y:
+#     print (" x is greater than y")
+#     if x >30:
+#         print("x is greater than 30")
+#     else:
+#         print ("x is not grater than 30")
+# else:
+#     print(" x is not greater than y")
+           
+
+#################################  loop - for #################################################
+
+# fruits=['apple', 'banan', 'cherry']
+# for x in fruits:
+#     print(x)
+    
+    
+#################################  loop - while   #################################################
+# until conditionils to be fila , it is exeuted
+
+
+# x=0
+# while x <= 10:
+#     print (x)
+#     x += 1
+
+# x=10
+# while x <= 101:
+#     print (x)
+#     x += 1
+           
+# count =0
+# while count <= 10:
+#     print (count)
+#     count += 1
+
+
+
+#################################  break - continue  #################################################
+
+# for i in range(10):
+#     if i ==5:
+#         break
+#     print (i)
+
+
+# for i in range(10):
+#     if i ==5:
+#        continue
+#     print (i)
+
+
+
+
+#################################  dictionary  #################################################
+
+# person= {'name':'ashok', 'age':'30', 'city':'bangalkore'}
+# for key,value  in person.items():
+#     print(f'{key}:{value}')
+
+
+#################################  else  with out break #################################################
+
+# for i in range(10):
+#     print(i)
+# else:
+#     print("loop exited with out a break")
+
+
+#################################   break  inside for loop  #################################################
+
+# for i in range(5):
+#     if i == 3:
+#         break  
+#         #print     # here print is not valid, under "if" we write print blkock
+#     print(i)
+# else:
+#     print("loop exited with out a break")
+ 
+
+
+#################################    #################################################
+
+
